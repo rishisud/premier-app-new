71bc88200a0cba03f01f666b4551246084656fa7
Santhosh <>
2022-02-16 16:48:08 +0530

    logout fix

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -16,6 +16,7 @@ export class AppHeaderComponent {
 	logOut() {
     this.authenticationService.logout();
     this.user = '';
-    this.router.navigate(['/login']);
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.navigate(['/']);
   }
 }
