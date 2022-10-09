5d0eabcca4352a92eca74dddc170ae457abae368
manithan <>
2022-10-09 21:50:53 +0530

    device interogation page redirect from dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-dashboard',
@@ -8,6 +9,8 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class DashboardComponent implements AfterViewInit {
 	role ='admin';
+	constructor(private _router: Router) { }
+
 	ngOnInit() {
 		this.role = sessionStorage.getItem('currentUser');
 	  }
@@ -16,5 +19,9 @@ export class DashboardComponent implements AfterViewInit {
 	public completedTabColumns = ['workorder_id', 'workorder_no', 'equipmentName', 'meNumber'];
 
 	public dataSource = new MatTableDataSource<any>();
+
+	redirectToDetails(element:any){
+		this._router.navigate(['/device-interrogation',element.meNumber,element.workorder_id]);
+	  }
 
 }
