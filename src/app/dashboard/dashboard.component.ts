0c258fb25b58d2dc20587167ab4d7aedb9ab158e
manithan <>
2022-10-09 23:04:57 +0530

    fetch workek order

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, AfterViewInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
 	selector: 'app-dashboard',
@@ -9,16 +11,25 @@ import { Router } from '@angular/router';
 })
 export class DashboardComponent implements AfterViewInit {
 	role ='admin';
-	constructor(private _router: Router) { }
-
-	ngOnInit() {
-		this.role = sessionStorage.getItem('currentUser');
-	  }
-	ngAfterViewInit() { }
+	BASE_URL = environment.API_BASE_URL;
 	public displayedColumns = ['workorder_id', 'workorder_no', 'equipmentName', 'meNumber', 'action'];
 	public completedTabColumns = ['workorder_id', 'workorder_no', 'equipmentName', 'meNumber'];
 
 	public dataSource = new MatTableDataSource<any>();
+	constructor(private httpClient: HttpClient,private _router: Router) { }
+
+	ngOnInit() {
+		this.role = sessionStorage.getItem('currentUser');
+		this.getAllWorkOrders();
+	}
+	
+	public getAllWorkOrders = () => {
+	  this.httpClient.get<any>(this.BASE_URL + '/workorder/details').subscribe(data =>{
+		this.dataSource.data = data;
+	  })
+	}
+
+	ngAfterViewInit() { }
 
 	redirectToDetails(element:any){
 		this._router.navigate(['/device-interrogation',element.meNumber,element.workorder_id]);
